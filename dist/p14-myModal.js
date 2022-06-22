072ccf7131541b9d33a262ae46b381928bae3b00
Mimi <87867561+Mimi1706@users.noreply.github.com>
2022-06-22 15:04:35 +0200

    Added github pages

--- dist/p14-myModal.js
+++ dist/p14-myModal.js
@@ -11,6 +11,13 @@ require("./p14-myModal.css");
 
 var _react = _interopRequireDefault(require("react"));
 
+/**
+ * 
+ * @param {boolean} props.display
+ * @param {string} props.message
+ * @param {function} props.action
+ * @returns {JSX} Modal component
+ */
 var Modal = function Modal(props) {
   return /*#__PURE__*/_react.default.createElement("div", {
     className: props.display === true ? "modal-window" : "hidden"
